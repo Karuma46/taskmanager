022294c3ad6f43aa37b61041df32e842332adf52
Steve Karuma <s.karuma@huqas.org>
2023-06-10 20:48:40 +0300

    Added theming

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./scss/main.scss";
 function App() {
 	return (
 		<div className="App">
-			<header>
-				<h1>Task Manager!</h1>
-			</header>
+			<h2>You've got 8 tasks today.</h2>
+			<h3>Hello Jessika!</h3>
+			<h4>Project Time Tracker</h4>
+			<a href="/">Dashboard</a>
+			<p>Make a landing page and mobile app</p>
 		</div>
 	);
 }
