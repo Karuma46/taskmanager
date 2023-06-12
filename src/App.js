aafd49a24b7381c7cc887d50560a68984c4a3d85
Steve Karuma <s.karuma@huqas.org>
2023-06-12 17:05:21 +0300

    Added the home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import Home from "./home";
 import "./scss/main.scss";
-import Sidebar from "./components/sidebar/sidebar";
-import Profile from "./components/profile/profile";
 
 function App() {
 	return (
 		<div className="App">
-			<Sidebar />
-			<Profile />
+			<Home />
 		</div>
 	);
 }
